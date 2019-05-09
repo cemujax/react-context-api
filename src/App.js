6ee88aea6b144a837da1ce88bbfbce9c9baf19b7
cemujax <cemujax@gmail.com>
2019-05-09 16:04:36 +0900

    Counter Context

--- src/App.js
+++ src/App.js
@@ -1,19 +1,29 @@
-import React, { Component } from "react";
+import React from "react";
 import LeftPane from "./components/LeftPane";
 import RightPane from "./components/RightPane";
-import { SampleProvider } from "./contexts/sample";
+import Counter from "./components/Counter";
 
-class App extends Component {
-  render() {
-    return (
-      <SampleProvider>
-        <div className="panes">
-          <LeftPane />
-          <RightPane />
-        </div>
-      </SampleProvider>
-    );
-  }
-}
+import { SampleProvider } from "./contexts/sample";
+import { AnotherProvider } from "./contexts/another";
+
+const AppProvider = ({ contexts, children }) =>
+  contexts.reduce(
+    (prev, context) =>
+      React.createElement(context, {
+        children: prev
+      }),
+    children
+  );
+const App = () => {
+  return (
+    <AppProvider contexts={[SampleProvider, AnotherProvider]}>
+      <div className="panes">
+        <LeftPane />
+        <RightPane />
+      </div>
+      <Counter />
+    </AppProvider>
+  );
+};
 
 export default App;
